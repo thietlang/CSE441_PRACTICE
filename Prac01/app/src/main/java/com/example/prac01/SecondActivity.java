5d2ac6432c7d2af261b7f4fa8b98d3d586ad936b
DESKTOP-SG5OL0L\xuant <xuanthuc123412@gmail.com>
2024-10-03 21:53:46 +0700

    add_secondactivity.java

--- Prac01/app/src/main/java/com/example/prac01/SecondActivity.java
+++ Prac01/app/src/main/java/com/example/prac01/SecondActivity.java
@@ -1,6 +1,9 @@
 package com.example.prac01;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
-
+    private EditText etFullName, etGpa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_second);
+        etFullName = findViewById(R.id.etFullName);
+        etGpa = findViewById(R.id.etGpa);
+        Button btnSubmit = findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(view -> {
+            String fullName = etFullName.getText().toString();
+            float gpa = Float.parseFloat(etGpa.getText().toString());
+
+            Intent resultIntent = new Intent();
+            resultIntent.putExtra("fullName", fullName);
+            resultIntent.putExtra("gpa", gpa);
+            setResult(RESULT_OK, resultIntent);
+            finish();
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
